431477b029ef7e59501d0ae14a9dd87b184c24de
MGorbachev <opti213@mail.ru>
2019-10-13 20:55:32 +0300

    linters

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from typing import Any
 
+import requests
+from loguru import logger
 from telebot import TeleBot
 from telebot.types import Message
-import requests
 
 from app.config import SECRET_TOKEN
 
@@ -15,22 +16,18 @@ def start_message(message: Message) -> Any:
 
 
 @bot.message_handler(commands=["help"])
-def start_message(message: Message) -> Any:
+def help_message(message: Message) -> Any:
     bot.send_message(message.chat.id, "you can say me: \nhello \njoke")
 
 
 @bot.message_handler(content_types=["text"])
 def send_message(message: Message) -> Any:
-    print(message.from_user.username)
-    print(message.text)
+    logger.debug(message.from_user.username)
+    logger.debug(message.text)
     if message.text.lower() == "joke":
-        r = requests.get("http://api.icndb.com/jokes/random")
-        return bot.send_message(message.chat.id, r.json()["value"]["joke"])
-    elif message.from_user.username.lower() == "nsidnev":
-        with open("doggo.jpg", "rb") as file:
-            return bot.send_photo(message.chat.id, file)
-    else:
-        return bot.send_message(message.chat.id, message.text)
+        resp = requests.get("http://api.icndb.com/jokes/random")
+        return bot.send_message(message.chat.id, resp.json()["value"]["joke"])
+    return bot.send_message(message.chat.id, message.text)
 
 
 bot.polling()
