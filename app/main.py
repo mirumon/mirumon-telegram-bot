48659d385721b17a91bb18179acfdd8f5af1401d
MGorbachev <opti213@mail.ru>
2019-10-16 21:29:50 +0300

    services

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ import requests
 from loguru import logger
 from telebot import TeleBot
 from telebot.types import Message
+from typing import List
 
 from app.config import API_BASE_URL, SECRET_TOKEN
 from app.resources.messages.common import HELLO, HELP
 from app.schemas.response_models import Computer
+from app.services.requests import get_all_computers
 
 bot = TeleBot(SECRET_TOKEN)
 
@@ -24,13 +26,7 @@ def help_message(message: Message) -> None:
 
 @bot.message_handler(commands=["computers"])
 def get_computers(message: Message) -> None:
-    resp = requests.get(API_BASE_URL + "/computers")
-    if resp.status_code != requests.codes.ok:
-        logger.error(resp.status_code)
-        bot.send_message(message.chat.id, "not ok response")
-        return
-    computers = [Computer(**computer) for computer in resp.json()]
-    computers.sort(key=lambda cmp: cmp.domain)
+    computers = get_all_computers()
     msg = ""
     for computer in computers:
         tmp_domain = ""
@@ -38,9 +34,6 @@ def get_computers(message: Message) -> None:
             msg += f"\n In domain {computer.domain}\n\n"
         msg += f"{computer.name} [{computer.username}]\n"
     bot.send_message(message.chat.id, msg)
-
-    resp = requests.get(GET_JOKE_API_URL)
-    bot.send_message(message.chat.id, resp.json()["value"]["joke"])
 
 
 @bot.message_handler(commands=["joke"])
