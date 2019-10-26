7e92830ca886c2db4c5343729f158ed61d71b533
Nik <snive2013@yandex.ru>
2019-10-26 19:53:20 +0300

    Feature/ci (#4)
    * change scripts
    
    * add actions and scripts for it
    
    * add dependencies for tests and test example
    
    * remove strange import and function that use it
    
    * add pydantic as dependency
    
    * fix pydantic version in lock
    
    * change ssh config and pass env vars into container

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
 import requests
-from loguru import logger
 from telebot import TeleBot
 from telebot.types import Message
-from typing import List
 
-from app.config import API_BASE_URL, SECRET_TOKEN
+from app.config import SECRET_TOKEN
 from app.resources.messages.common import HELLO, HELP
-from app.schemas.response_models import Computer
-from app.services.requests import get_all_computers
 
 bot = TeleBot(SECRET_TOKEN)
 
@@ -22,18 +18,6 @@ def start_message(message: Message) -> None:
 @bot.message_handler(commands=["help"])
 def help_message(message: Message) -> None:
     bot.send_message(message.chat.id, HELP)
-
-
-@bot.message_handler(commands=["computers"])
-def get_computers(message: Message) -> None:
-    computers = get_all_computers()
-    msg = ""
-    for computer in computers:
-        tmp_domain = ""
-        if computer.domain != tmp_domain:
-            msg += f"\n In domain {computer.domain}\n\n"
-        msg += f"{computer.name} [{computer.username}]\n"
-    bot.send_message(message.chat.id, msg)
 
 
 @bot.message_handler(commands=["joke"])
---
+++ tests/test_example.py
@@ -0,0 +1,2 @@
+def test_2_plus_2():
+    assert 2 + 2 == 4
