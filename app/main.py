77d060a933d76cbb7fca623efe178145ecd4f0c4
Sidnev Nikolaj <nsidnev@ccsteam.ru>
2019-10-26 16:03:20 +0300

    remove strange import and function that use it

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from telebot.types import Message
 
 from app.config import SECRET_TOKEN
 from app.resources.messages.common import HELLO, HELP
-from app.services.requests import get_all_computers
 
 bot = TeleBot(SECRET_TOKEN)
 
@@ -19,18 +18,6 @@ def start_message(message: Message) -> None:
 @bot.message_handler(commands=["help"])
 def help_message(message: Message) -> None:
     bot.send_message(message.chat.id, HELP)
-
-
-@bot.message_handler(commands=["computers"])
-def get_computers(message: Message) -> None:
-    computers = get_all_computers()
-    msg = ""
-    for computer in computers:
-        tmp_domain = ""
-        if computer.domain != tmp_domain:
-            msg += f"\n In domain {computer.domain}\n\n"
-        msg += f"{computer.name} [{computer.username}]\n"
-    bot.send_message(message.chat.id, msg)
 
 
 @bot.message_handler(commands=["joke"])
