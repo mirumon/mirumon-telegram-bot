9911e1203a980f82ddb00bcf8cb236369ce59d73
Opti213 <31591162+Opti213@users.noreply.github.com>
2019-11-17 04:19:01 +0300

    Feature/variables (#7)

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings, HttpUrl, SecretStr
 
 
 class Settings(BaseSettings):
-    tg_api_id: SecretStr
-    tg_api_hash: SecretStr
+    tg_app_id: SecretStr
+    tg_hash: SecretStr
     tg_bot_token: SecretStr
     api_base_url: HttpUrl
 
