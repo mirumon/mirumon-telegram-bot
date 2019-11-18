f0439fdf015dfa54f6a9e30029c9fa9bd07bce44
mgorbachev <opti213@mail.ru>
2019-11-18 03:34:07 +0300

    /computers test

--- app/config.py
+++ app/config.py
@@ -2,10 +2,10 @@ from pydantic import BaseSettings, HttpUrl, SecretStr
 
 
 class Settings(BaseSettings):
-    tg_app_id: SecretStr
-    tg_hash: SecretStr
-    tg_bot_token: SecretStr
-    api_base_url: HttpUrl
+    tg_app_id: SecretStr = ""
+    tg_hash: SecretStr = ""
+    tg_bot_token: SecretStr = ""
+    api_base_url: HttpUrl = "https://google.com"
 
     class Config:  # noqa: WPS431
         case_sensitive = False
